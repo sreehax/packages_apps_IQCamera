835e38aadd00bdd1354f816096e68d5adab1a760
Sreehari S <sreeharisreedev1@gmail.com>
2018-07-11 17:14:15 -0700

    Initial Commit

---
+++ Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v13
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := IQCamera
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_USE_AAPT2 := true
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PACKAGE)
+include $(CLEAR_VARS)
